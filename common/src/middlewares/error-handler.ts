2f1db12e6d2d75ae2857537f1a2a4260a2bd94c1
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2022-06-06 19:32:51 +0900

    Updates

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -6,5 +6,7 @@ export const errorHandler = (err: Error, req: Request, res: Response, next: Next
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
+  console.error(err);
+
   res.status(400).send({ errors: [{ message: '어딘가 오류났는데 모르겠어..' }] });
 };
